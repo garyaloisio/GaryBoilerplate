2ad2c0563521fd20382fb422865e572357892a22
Zach Resnick <zachary.l.resnick@gmail.com>
2020-04-01 22:46:20 -0400

    Adds some example code for Minashi

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,10 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import example from './example'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, example}) //note that the state being exported from the example reducer is being imported as example, so to access the data we got in the thunk, you would go to example.data inside a component.
+
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -12,3 +14,4 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './example'
